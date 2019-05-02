97578709d44ac5b4757034658d1416d55b54cc80
Robin Reuteler <reutelerr@hotmail.com>
2019-05-02 13:14:19 +0200

    TODO : Entrées utilisateur, formatage des sorties

--- Random/main.c
+++ Random/main.c
@@ -10,25 +10,55 @@
 #include <stdlib.h>
 #include <time.h>
 
-void generateDistribution(unsigned ValueRange, unsigned ThrowNum)
+void showDistributionLine(unsigned PercentValue)
 {
-    int distributionTab[ValueRange];
-    for(unsigned i=0; i<ValueRange; ++i)
+    printf("%d       ", PercentValue);
+    for(size_t i=0; i<PercentValue; ++i)
     {
-        *(distributionTab+i) = 0;
-    }
-    for(unsigned i=0; i<ThrowNum; ++i)
-    {
-        (*(distributionTab+rand()%ValueRange))++;
+        printf("=");
     }
 }
 
-void showDistributionLine();
+void showDistribution(double* distributionTab, unsigned UpperBound)
+{
+    for(size_t i=0; i<UpperBound; ++i)
+    {
+        printf("tableau [%zu]     ", i);
+        showDistributionLine(*(distributionTab+i));
+        printf("\n");
+    }
+}
 
-void showDistribution();
+void generateDistribution(unsigned UpperBound, unsigned ThrowNum)
+{
+    double distributionTab[UpperBound];
+    //Initialisation à 0
+    for(unsigned i=0; i<UpperBound; ++i)
+    {
+        *(distributionTab+i) = 0;
+    }
+    //Compte le nombre de lancers pour chaque valeur
+    for(unsigned i=0; i<ThrowNum; ++i)
+    {
+        (*(distributionTab+rand()%UpperBound)) ++;
+    }
+    //Divise (division décimale) chaque élément du tableau par le nombre de lancers, puis multiplie par 100 pour obtenir le pourcentage
+    for(unsigned i=0; i<UpperBound; ++i)
+    {
+        *(distributionTab+i) = (*(distributionTab+i)/(double)(ThrowNum))*100;
+    }
+    
+    showDistribution(distributionTab, UpperBound);
+    
+}
+
+
+
+
 
 int main()
 {
     srand((unsigned)time(NULL));
+    generateDistribution(10, 1000);
     return 0;
 }
